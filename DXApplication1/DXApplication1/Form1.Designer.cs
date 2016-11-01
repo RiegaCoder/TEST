887083a03ff96da2f78e1149f53c55b2585abc53
david.riega <david.riega@aquafeedhn.com>
2016-11-01 12:01:27 -0600

    Commit DRiega

--- DXApplication1/DXApplication1/Form1.Designer.cs
+++ DXApplication1/DXApplication1/Form1.Designer.cs
@@ -28,20 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Location = new System.Drawing.Point(28, 45);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(346, 219);
+            this.groupControl1.TabIndex = 0;
+            this.groupControl1.Text = "groupControl1";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(283, 304);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "asdasdasd";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 278);
+            this.ClientSize = new System.Drawing.Size(558, 393);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.groupControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
 
     }
 }
--- DXApplication1/DXApplication1/Form1.cs
+++ DXApplication1/DXApplication1/Form1.cs
@@ -15,5 +15,10 @@ namespace DXApplication1
         {
             InitializeComponent();
         }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola MotherFucker!!!!");
+        }
     }
 }
