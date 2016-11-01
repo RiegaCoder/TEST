5c97fa32ca29b04a3c7d78286ec6957cf731ba6d
jvaldiviezo <jesus.valdiviezo@aquafeedhn.com>
2016-11-01 11:52:00 -0600

    Prueba de merge1

--- DXApplication1/DXApplication1/Form1.cs
+++ DXApplication1/DXApplication1/Form1.cs
@@ -15,5 +15,10 @@ namespace DXApplication1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola Mundo");
+        }
     }
 }
